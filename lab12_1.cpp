c56f9267ec1c63d3b13686f79babb416cd5a976a
tensept <vankaew582gmail.com>
2023-01-06 15:24:44 +0700

    lab12_1 compeleted

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,29 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    int i = 0;
+    while(i<3){
+        cin.get();
+        i++;
+        }
+    srand(time(0));
+    int x = rand()%9;
+        if(x == 0) cout << "You will get A in this 261102.";
+        else if(x == 1) cout << "You will get B+ in this 261102.";
+        else if(x == 2) cout << "You will get B in this 261102.";
+        else if(x == 3) cout << "You will get C+ in this 261102.";
+        else if(x == 4) cout << "You will get C in this 261102.";
+        else if(x == 5) cout << "You will get D+ in this 261102.";
+        else if(x == 6) cout << "You will get D in this 261102.";
+        else if(x == 7) cout << "You will get F in this 261102.";
+        else cout << "You will get W in this 261102.";
+
+    
+
+    return 0;
+
+}
